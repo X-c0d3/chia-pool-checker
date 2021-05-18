1a0b35f1fd2d749c604c79c2d7f22d80b8470dce
Watchara Pongsri <giffyp@hotmail.com>
2021-05-19 01:32:34 +0700

    add node-cron

--- build/src/constants/Constants.js
+++ build/src/constants/Constants.js
@@ -5,7 +5,8 @@ const AppConfig = {
     PAGESIZE: process.env.PAGESIZE,
     API_URL: process.env.API_URL,
     AUTH_TOKEN: process.env.AUTH_TOKEN,
-    XCH_MARKET_PRICE: process.env.XCH_MARKET_PRICE,
-    LINE_TOKEN: process.env.LINE_TOKEN
+    ENABLE_LINE_NOTIFY: process.env.ENABLE_LINE_NOTIFY,
+    LINE_TOKEN: process.env.LINE_TOKEN,
+    CURRENCY: process.env.CURRENCY
 };
 exports.AppConfig = AppConfig;
--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,21 @@
   # Web Site: https://www.rockdevper.com
 */
 
+import * as cronJob from 'node-cron'
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { AppConfig } from './constants/Constants';
 import { getHpoolOnline, getRevenue, getMiner, getAssets, getMiningIncome } from './util/Hpool';
 import { sendLineNotify } from './util/LineNotify';
-import { getPriceMarketCap } from './util/MarketCap'
+import { getPriceMarketCap } from './util/MarketCap';
 
 
 // Market price from https://coinmarketcap.com/currencies/chia-network/
-const run = async () => {
+const runTask = async () => {
   var pricevalue = await getPriceMarketCap('chia-network');
-  let marketPrice  = parseFloat(pricevalue.replace(/[฿]/g, m => '').replace(/[$]/g, m => '').replace(/[,]/g, m => ''));
-  
+  let marketPrice = parseFloat(pricevalue.replace(/[฿]/g, m => '').replace(/[$]/g, m => '').replace(/[,]/g, m => ''));
+
   var poolOnline = await getHpoolOnline(marketPrice);
   var revenue = await getRevenue(marketPrice);
   var workers = await getMiner();
@@ -27,7 +28,30 @@ const run = async () => {
     sendLineNotify(`${poolOnline} ${revenue} ${workers}`);
 };
 
-run();
+
+const jobs = [
+  {
+    pattern: '30 08 * * *',
+    message: 'Recheck hpool 08:30'
+  }, {
+    pattern: '0 12 * * *',
+    message: 'Recheck hpool in 12:00'
+  }, {
+    pattern: '0 18 * * *',
+    message: 'Recheck hpool 18:00'
+  }
+];
+
+console.log('Add task');
+jobs.forEach((job, index) => {
+  console.log(`[+${index}] <<${job.pattern}>> - ${job.pattern}`)
+  cronJob.schedule(job.pattern, () => {
+    console.log(job.message);
+    runTask();
+  }).start();
+
+});
+console.log('Task Started')
 
 
-
+//runTask();
